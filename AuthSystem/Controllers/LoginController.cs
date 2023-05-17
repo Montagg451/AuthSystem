7d2a8a0859039228f618623d08e9221f255ac43b
Vinicius <102189475+Montagg451@users.noreply.github.com>
2023-05-16 23:14:58 -0300

    Ajust login 1.1, Config Database

---
+++ AuthSystem/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace AuthSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logar(string username, string senha)
+        {
+            MySqlConnection mySqlConnections = new MySqlConnection("server=localhost;database=usuariosdb;uid=root;password=3966458");
+            await mySqlConnections.OpenAsync();
+
+            MySqlCommand mySqlCommand = mySqlConnections.CreateCommand();
+            mySqlCommand.CommandText = $"Select * FROM usuarios WHERE username = '{username}'AND senha = '{senha}'";
+
+            MySqlDataReader reader = mySqlCommand.ExecuteReader();
+
+                if(await reader.ReadAsync()) 
+                {
+                    return Json(new { Msg = "Usuário Logado com sucesso!"});
+                }
+            
+            return Json(new { Msg = "Usuário Não encontrado! verifique suas credenciais" });
+        }
+    }
+}
