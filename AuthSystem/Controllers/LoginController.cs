10fd29e2e36bb0e2991152cb1f70e961e114daa9
Vinicius <102189475+Montagg451@users.noreply.github.com>
2023-05-18 12:47:35 -0300

    Ajust Login 1.2, Ajust Database 1.0

--- AuthSystem/Controllers/LoginController.cs
+++ AuthSystem/Controllers/LoginController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
+using System.Security.Claims;
 
 namespace AuthSystem.Controllers
 {
@@ -7,6 +9,11 @@ namespace AuthSystem.Controllers
     {
         public IActionResult Index()
         {
+            if(User.Identity.IsAuthenticated)
+            {
+                return Json(new { Msg = "Usuário já logado!"});
+            }
+
             return View();
         }
 
@@ -23,10 +30,32 @@ namespace AuthSystem.Controllers
 
                 if(await reader.ReadAsync()) 
                 {
+                    int usuarioId = reader.GetInt32(0);
+                    string nome = reader.GetString(1);
+
+                List<Claim> direitosAcesso = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.NameIdentifier,usuarioId.ToString()),
+                        new Claim(ClaimTypes.Name,nome)
+                    };
+
+                    var identity = new ClaimsIdentity(direitosAcesso,"identity.Login");
+                    var userPrincipal = new ClaimsPrincipal(new[] { identity });
+
+                    await HttpContext.SignInAsync(userPrincipal);
+
                     return Json(new { Msg = "Usuário Logado com sucesso!"});
                 }
             
             return Json(new { Msg = "Usuário Não encontrado! verifique suas credenciais" });
+        }
+        public async Task<IActionResult> Logout()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                await HttpContext.SignOutAsync();
+            }
+            return RedirectToAction("Index", "Login");
         }
     }
 }
